97eda26dff888de70e413883fd81ec1cf14952a8
Shane Breining <sfbreining@gmail.com>
2021-08-25 13:18:11 -0600

    Update migrate.js

--- migrate.js
+++ migrate.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 try {
-    fs.openSync('./database.db', 'ax+');
+  fs.openSync('./database.db', 'ax+');
 } catch (err) {
-    console.log('Database file already exists.');
+  console.log('Database file already exists.');
 }
 
 const SQL = require('better-sqlite3');
@@ -11,36 +11,37 @@ const SQL = require('better-sqlite3');
 const db = new SQL('./database.db', { verbose: console.log });
 
 try {
-    const createPosts = "CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, post_name CHAR(50) NOT NULL);"
-    db.exec(createPosts);
+  const createPosts = 'CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, post_name CHAR(50) NOT NULL);';
+  db.exec(createPosts);
 } catch (err) {
-    console.log('The table, posts, already exists.');
+  console.log('The table, posts, already exists.');
 }
 
 try {
-    const createTimes = "CREATE TABLE times (id INTEGER PRIMARY KEY AUTOINCREMENT, next_post TEXT NOT NULL)";
-    db.exec(createTimes);
+  const createTimes = 'CREATE TABLE times (id INTEGER PRIMARY KEY AUTOINCREMENT, next_post TEXT NOT NULL)';
+  db.exec(createTimes);
 } catch (err) {
-    console.log('The table, times, already exists.');
+  console.log('The table, times, already exists.');
 }
 
 const results = db.prepare('SELECT * FROM times WHERE id = ?').get(1);
 
 if (!results) {
-    const now = new Date();
-    now.setHours(9);
-    now.setMinutes(0);
-    now.setSeconds(0);
-    now.setMilliseconds(0);
+  const now = new Date();
+  now.setHours(9);
+  now.setMinutes(0);
+  now.setSeconds(0);
+  now.setMilliseconds(0);
 
-    db.prepare("INSERT INTO times (next_post) VALUES (?)").run(now.toISOString());
+  db.prepare('INSERT INTO times (next_post) VALUES (?)').run(now.toISOString());
 } else {
-    console.log('A row of ID 1 already exists, no need for anymore rows.');
+  console.log('A row of ID 1 already exists, no need for anymore rows.');
 }
 
 try {
-    const createLogs = "CREATE TABLE logs (id INTEGER PRIMARY KEY AUTOINCREMENT, level TEXT NOT NULL DEFAULT 'INFO', log TEXT NOT NULL, created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP);";
-    db.exec(createLogs);
+  const createLogs =
+    "CREATE TABLE logs (id INTEGER PRIMARY KEY AUTOINCREMENT, level TEXT NOT NULL DEFAULT 'INFO', log TEXT NOT NULL, created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP);";
+  db.exec(createLogs);
 } catch (err) {
-    console.log('The table, logs, already exists.');
+  console.log('The table, logs, already exists.');
 }
